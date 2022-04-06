9e35123bf31d7cad4fdb17768330a80ba78fe79a
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-04-06 18:07:19 -0400

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -1,10 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-#define sync ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-#define INF (int)1e8
-#define endl '\n'
+#define sync ios_base::sync_with_stdio(false),cin.tie(0)
+#define pb       push_back
+#define mp       make_pair
+#define all(x)   x.begin(), x.end()
+#define fi       first
+#define se       second
+#define cl       clear
 
 int32_t main() {
 	return 0;
